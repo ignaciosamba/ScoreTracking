012f62d9622ed22a44a4b98d22835e7644e7412a
ignacio sambataro <ignaciosamba@gmail.com>
2022-11-12 13:46:22 +0100

    - key

--- app/src/main/java/com/example/scoretracking/network/ApiValues.kt
+++ app/src/main/java/com/example/scoretracking/network/ApiValues.kt
@@ -1,6 +1,6 @@
 package com.example.scoretracking.network
 
-const val API_KEY = "50130162"
+const val API_KEY = ""
 const val SPORT_API_BASE_URL = "https://www.thesportsdb.com/api/"
 const val ESPN_SPORT_API_BASE_URL = "https://site.api.espn.com/apis/v2/sports/"
 
